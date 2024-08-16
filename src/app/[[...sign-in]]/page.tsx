927767f28c2ca25053f1d3d641014f76f9a6082e
ArsenalBAck2004 <utphuongmap@gmail.com>
2024-08-15 22:36:39 -0400

    sign in and sign up folder

---
+++ src/app/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+
+import { SignUp } from '@clerk/nextjs';
+
+
+export default function Page() {
+  return (
+    <div className='flex items-center justify-center flex-col gap-10'>
+      <h1 className='text-4xl font-bold mt-20'>This is sign up page</h1>
+      <SignUp />
+    </div>
+  );
+}
